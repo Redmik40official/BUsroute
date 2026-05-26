8de599389bc0c6d9443eafd4f13575eb66d1e074
AURCM Dev <developer@example.com>
2026-05-26 14:25:12 +0530

    Fix uuid ESM require error

--- backend/src/modules/auth/auth.service.js
+++ backend/src/modules/auth/auth.service.js
@@ -15,7 +15,8 @@
 "use strict";
 
 const bcrypt = require("bcryptjs");
-const { v4: uuidv4 } = require("uuid");
+const crypto = require("crypto");
+const uuidv4 = () => crypto.randomUUID();
 const db = require("../../../data/database");
 const { signAccessToken, signRefreshToken, verifyRefreshToken } = require("../../shared/jwt.util");
 
